da7b972504a2d0f80160c6af37ba147a1163b6fa
Brennan McFarland <bfm21@case.edu>
2019-09-28 17:06:52 -0400

    changed a ton of stuff

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ def run():
 
     # TODO: create objects that exist at start here
     object_list = [
+        objects.Object(rendering.render_context.CreateSquare(position=(200, 200), size=(60, 60))),
         objects.Object(rendering.render_context.CreateSquare(position=(200, 200), size=(60, 60)))
     ]
     global running
     running = True
     while running:
-        rendering.clear()
         result = input.handle_input()
         if result == 'QUIT':
             running = False
+        rendering.clear()
         [o.graphic.render(rendering.render_context) for o in object_list]
-        rendering.render_context.sdl_renderer.render([o.graphic.sprite for o in object_list])
+        #rendering.render_context.sdl_renderer.render([o.graphic.sprite for o in object_list])
+        rendering.swap()
         object_list[0].moveAdditive(1, 1)
         if input.is_mouse_down:
             object_list[0].move(input.x.value, input.y.value)
--- rendering.py
+++ rendering.py
@@ -6,19 +6,9 @@ import sdl2.ext
 debug = True
 
 WHITE = sdl2.ext.Color(255, 255, 255)
-RED = sdl2.ext.Color(255, 0, 0)
 BLACK = sdl2.ext.Color(0, 0, 0)
 
 window, render_context = None, None
-
-
-class SoftwareRenderer(sdl2.ext.SoftwareSpriteRenderSystem):
-    def __init__(self, window):
-        super(SoftwareRenderer, self).__init__(window)
-
-    def render(self, components):
-        sdl2.ext.fill(self.surface, BLACK)
-        super(SoftwareRenderer, self).render(components)
 
 
 class RenderContext:
@@ -26,38 +16,28 @@ class RenderContext:
     def __init__(self, window):
         self.window = window
         self.world = sdl2.ext.World()
-        self.sdl_renderer = sdl2.ext.TextureSpriteRenderSystem(self.window)
-        self.sprite_factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=self.sdl_renderer)
-
+        self.sdl_renderer = sdl2.SDL_CreateRenderer(window, -1, sdl2.SDL_RENDERER_ACCELERATED)
 
     def CreateSquare(self, position=(0, 0), size=(20, 20), color=WHITE):
-        square_sprite = self.sprite_factory.from_color(color, size=size)
-        square = Renderable(self.world, square_sprite, position[0], position[1])
+        square = Renderable(self.world, position[0], position[1])
         return square
 
-    def CreatePoint(self, position=(0, 0), color=WHITE):
-        #sdl2.SDL_RenderDrawPoint(self.sdl_renderer.sdlrenderer, position[0], position[1])
-        point_sprite = self.sprite_factory.create_texture_sprite(self.sdl_renderer._renderer, (10, 10), access=sdl2.SDL_TEXTUREACCESS_TARGET)
 
-        return Renderable(self.world, point_sprite, position[0], position[1])
-
-
-class Renderable(sdl2.ext.Entity):
-    def __init__(self, world, sprite, posx=0, posy=0):
-        self.sprite = sprite
-        self.sprite.x = posx
-        self.sprite.y = posy
+class Renderable:
+    def __init__(self, world, posx=0, posy=0):
+        self.x = posx
+        self.y = posy
 
     def Move(self, x, y):
-        self.sprite.x = x
-        self.sprite.y = y
+        self.x = x
+        self.y = y
 
     def MoveAdditive(self, x, y):
-        self.sprite.x += x
-        self.sprite.y += y
+        self.x += x
+        self.y += y
 
     def render(self, render_context):
-        render_context.sdl_renderer.render(self.sprite)
+        sdl2.SDL_RenderDrawRect(render_context.sdl_renderer, sdl2.SDL_Rect(0, 0, 100, 100))
 
 
 def init_rendering():
@@ -65,12 +45,24 @@ def init_rendering():
     global window, render_context
 
     # initialize window
-    sdl2.ext.init()
-    window = sdl2.ext.Window("Peer 2 Paint", size=(800, 800))
-    window.show()
+    sdl2.SDL_Init(sdl2.SDL_INIT_EVERYTHING)
+    #window = sdl2.ext.Window("Peer 2 Paint", size=(800, 800))
+    window = sdl2.SDL_CreateWindow("Peer 2 Paint".encode('utf-8'), sdl2.SDL_WINDOWPOS_CENTERED, sdl2.SDL_WINDOWPOS_CENTERED, 800, 800, 0)
+    #window.show()
 
     render_context = RenderContext(window)
 
 
 def clear():
-    sdl2.SDL_RenderClear(render_context.sdl_renderer.sdlrenderer)
+    sdl2.SDL_SetRenderDrawColor(render_context.sdl_renderer, 0, 0, 0, 255)
+    sdl2.SDL_RenderClear(render_context.sdl_renderer)
+    sdl2.SDL_SetRenderDrawColor(render_context.sdl_renderer, 255, 255, 255, 255)
+
+def swap():
+
+    #square_sprite = sdl2.SDL_CreateTexture(render_context.sdl_renderer, sdl2.SDL_PIXELFORMAT_RGB888,
+    #                                       sdl2.SDL_TEXTUREACCESS_STATIC, 100, 100)
+    #sdl2.SDL_SetTextureBlendMode(square_sprite, sdl2.SDL_BLENDMODE_NONE)
+    #sdl2.SDL_RenderCopy(render_context.sdl_renderer, square_sprite, sdl2.SDL_Rect(0, 0, 100, 100), sdl2.SDL_Rect(0, 0, 100, 100))
+    #sdl2.SDL_RenderDrawRect(render_context.sdl_renderer, sdl2.SDL_Rect(0, 0, 100, 100))
+    sdl2.SDL_RenderPresent(render_context.sdl_renderer)
