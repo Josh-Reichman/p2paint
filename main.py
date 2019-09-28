64b503fe272a778361ec61ee820fc0a555041304
Josh Reichman <whodaman101@gmail.com>
2019-09-28 11:57:21 -0400

    Integrated rendering into objects.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ running = True
 def run():
     rendering.init_rendering()
     # TODO: create objects that exist at start here
+    test_object = objects.Object(rendering.render_context.CreateSquare(position=(200, 200), size=(60, 60)))
     #square2 = render_context.CreateSquare(position=(200, 200), size=(60, 60))
     global running
     running = True
